3597dffcd46d7ade47b4369f09b8afa5566b39e7
Dmytro Manankin <dmitry.manankin@gmail.com>
2024-08-05 15:22:42 +0300

    remove comments

--- features/runTest.js
+++ features/runTest.js
@@ -1,8 +1,5 @@
 export default function runTest(main, test, slider) {
-    // document.querySelector('html').style.maxHeight = 'fit-content';
-    main.style.display = "none";
-    // page.style.height = "100vh";
-    // page.style.overflow = 'hidden';
-    test.style.display = "initial";
-    slider.setProgress(0);
+  main.style.display = "none";
+  test.style.display = "initial";
+  slider.setProgress(0);
 }
